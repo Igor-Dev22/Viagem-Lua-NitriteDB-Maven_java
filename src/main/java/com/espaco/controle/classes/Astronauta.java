5efd173f3ba897da18cccb8d1b4b2fbe39cbb909
Anderson Rodrigues <irdor.redna@gmail.com>
2025-11-23 15:46:44 -0300

    classe astronauta criada

--- src/main/java/com/espaco/controle/classes/Astronauta.java
+++ src/main/java/com/espaco/controle/classes/Astronauta.java
@@ -1,5 +1,21 @@
-package main.java.com.espaco.controle.classes;
+package com.espaco.controle.modelo;
 
-public class Astronauta {
-    
+import java.io.Serializable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.dizitart.no2.objects.Id;
+
+@Data
+@NoArgsConstructor
+public class Astronauta implements Serializable {
+    @Id
+    private Long id;
+    private String nome;
+    private String especialidade; // Ex: Piloto, Biólogo
+
+    public Astronauta(String nome, String especialidade) {
+        this.id = System.nanoTime(); // Gera ID único automaticamente
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
 }
